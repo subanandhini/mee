dbd9430074630cdc4dcfda1508da6aedd399a459
subanandhini <34816663+subanandhini@users.noreply.github.com>
2018-03-13 17:59:46 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class fact{
+public static void main(String args){
+Scanner sc=new Scanner(System.in);
+int a= sc.nextInt();
+int b=0;
+for(i=0;i<a;i++){
+b*=i;}
+return b;}}
